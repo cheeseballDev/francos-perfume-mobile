e56d1de8459d63a285b5fac6dcd217e8558a2ce5
Adidoodoo <zachrider09@gmail.com>
2026-09-13 14:19:19 +0800

    UPDATES - Added a function in RetrofitClient to get, save, and clear access tokens of a logged in account - Added an interceptor in RetrofitClient.getInstance to pass the access token around the endpoints - Added SessionManager that works as both the equivalent of ClaimsGetter in the ASP.NET backend and the session manager of the application

--- app/src/main/java/com/example/francosperfumemobile/backend/retrofit/RetrofitClient.java
+++ app/src/main/java/com/example/francosperfumemobile/backend/retrofit/RetrofitClient.java
@@ -1,5 +1,11 @@
 package com.example.francosperfumemobile.backend.retrofit;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -8,19 +14,68 @@ public class RetrofitClient {
     private static Retrofit retrofit;
 
     private static final String BASE_URL = "http://192.168.254.195:5000/";
+    private static SharedPreferences sharedPrefs;
 
+
+    public static void init(Context context) {
+        sharedPrefs = context.getSharedPreferences(
+                "auth",
+                Context.MODE_PRIVATE
+        );
+    }
+
+    public static String getAuthToken(){
+        if(sharedPrefs == null){
+            return "";
+        }
+        return sharedPrefs.getString("access_token", "");
+    }
+
+    public static void saveAuthToken(String token){
+        if(sharedPrefs == null){
+            return;
+        }
+
+        sharedPrefs.edit()
+                .putString("access_token", token)
+                .apply();
+    }
+
+    public static void clearToken(){
+        if(sharedPrefs == null){
+            return;
+        }
+
+        sharedPrefs.edit()
+                .remove("access_token")
+                .apply();
+    }
     public static Retrofit getInstance() {
 
         if(retrofit == null){
+            Interceptor authInterceptor = chain -> {
+                Request originalRequest = chain.request();
+                String token = getAuthToken();
+                Request.Builder requestBuilder = originalRequest.newBuilder();
+                if(token != null && !token.isEmpty()){
+                    requestBuilder.header("Authorization", "Bearer " + token);
+                }
+
+                Request request = requestBuilder.build();
+                return chain.proceed(request);
+            };
+
+            OkHttpClient okHttpClient =  new OkHttpClient.Builder()
+                        .addInterceptor(authInterceptor)
+                        .build();
 
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .client(okHttpClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
-
         }
 
         return retrofit;
     }
-
 }
---
+++ app/src/main/java/com/example/francosperfumemobile/backend/retrofit/SessionManager.java
@@ -0,0 +1,65 @@
+package com.example.francosperfumemobile.backend.retrofit;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SessionManager {
+    private static final String PREFS_NAME = "francos_perfume_session";
+    private static final String KEY_ACCESS_TOKEN = "access_token";
+    private static final String KEY_EMPLOYEE_ID = "employee_id";
+    private static final String KEY_BRANCH_ID = "branch_id";
+    private static final String KEY_ROLE = "role";
+    private static final String KEY_REQUIRES_PW_CHANGE = "requires_pw_change";
+    private final SharedPreferences sharedPrefs;
+
+    public SessionManager(Context context){
+        sharedPrefs = context.getSharedPreferences(PREFS_NAME, context.MODE_PRIVATE);
+    }
+
+    public void createSession(String accessToken, int employeeId, int branchId, String role, boolean passwordChange){
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString(KEY_ACCESS_TOKEN, accessToken);
+        editor.putInt(KEY_EMPLOYEE_ID, employeeId);
+        editor.putInt(KEY_BRANCH_ID, branchId);
+        editor.putString(KEY_ROLE, role);
+        editor.putBoolean(KEY_REQUIRES_PW_CHANGE, passwordChange);
+        editor.apply();
+    }
+
+    public void updateAccessToken(String newToken) {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString(KEY_ACCESS_TOKEN, newToken);
+        editor.apply();
+    }
+
+    public void clearSession() {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.clear();
+        editor.apply();
+    }
+
+
+    public String getAccessToken() {
+        return sharedPrefs.getString(KEY_ACCESS_TOKEN, null);
+    }
+
+    public int getEmployeeId() {
+        return sharedPrefs.getInt(KEY_EMPLOYEE_ID, -1);
+    }
+
+    public int getBranchId() {
+        return sharedPrefs.getInt(KEY_BRANCH_ID, -1);
+    }
+
+    public String getRole() {
+        return sharedPrefs.getString(KEY_ROLE, null);
+    }
+
+    public boolean getRequiresPwChange() {
+        return sharedPrefs.getBoolean(KEY_REQUIRES_PW_CHANGE, false);
+    }
+
+    public boolean isLoggedIn() {
+        return getAccessToken() != null && !getAccessToken().isEmpty();
+    }
+}
